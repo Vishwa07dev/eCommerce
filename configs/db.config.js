1fc8d2ba6c1cffc8041ab2bbf25fb6074b599c6a
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-02-13 14:33:09 +0530

    typo fix

--- configs/db.config.js
+++ configs/db.config.js
@@ -7,7 +7,7 @@ module.exports = {
     pool :{
         max :5,
         min :0,
-        acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+        acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
         idle :10000  // maximum time in ms that a connection can be idle before being released
     }
 }
