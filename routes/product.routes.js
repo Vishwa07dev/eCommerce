4a25fe5453236d30374e06227ee548bd456b6f7d
Kumar <m.kumarmohit19@gmail.com>
2022-03-22 09:30:20 +0530

    product routes middleware restored

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -9,7 +9,7 @@
  module.exports = function(app){
      
      //Route for the POST request to create the product
-     app.post("/ecomm/api/v1/products", productController.create);
+     app.post("/ecomm/api/v1/products",[requestValidator.validateProductRequest, authJwt.verifyToken,authJwt.isAdmin], productController.create);
  
      //Route for the GET request to fetch all the products
      app.get("/ecomm/api/v1/products", productController.findAll);
@@ -18,16 +18,16 @@
      app.get("/ecomm/api/v1/products/:id", productController.findOne);
  
      //Route for the PUT request to update a product based on the id
-     app.put("/ecomm/api/v1/products/:id", productController.update);
+     app.put("/ecomm/api/v1/products/:id",[requestValidator.validateProductRequest, authJwt.verifyToken,authJwt.isAdmin], productController.update);
  
      //Route for the DELETE request to delete a product based on the id
-     app.delete("/ecomm/api/v1/products/:id", productController.delete);
+     app.delete("/ecomm/api/v1/products/:id",[authJwt.verifyToken,authJwt.isAdmin], productController.delete);
 
      //Route for getting the list of products with cost greater than the 
-     app.get("/ecomm/api/v1/categories/:categoryId/products", productController.getProductsUnderCategory);
+     app.get("/ecomm/api/v1/categories/:categoryId/products",[requestValidator.validateCategoryPassedInReqParam], productController.getProductsUnderCategory);
      
      //Route for getting the list of products under a category
-     app.get("/ecomm/api/v1/categories/:categoryId/products", productController.getProductsUnderCategory);
+     app.get("/ecomm/api/v1/categories/:categoryId/products",[requestValidator.validateCategoryPassedInReqParam], productController.getProductsUnderCategory);
 
      
  }
